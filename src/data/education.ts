7e643ea61e1f2a7cb78e480af0418f4ef782d610
afrizalmuh <afrizalmuhardianto26@gmail.com>
2026-05-29 07:56:40 +0700

    refactor: update date education

--- src/data/education.ts
+++ src/data/education.ts
@@ -14,20 +14,22 @@ export const education: EducationEntry[] = [
     institution: "Binus University",
     degree: "Bachelor of Computer Science",
     field: "Informatics Engineering",
-    startDate: "Sep 2023",
-    endDate: "Jul 2025",
+    startDate: "Nov 2022",
+    endDate: "Nov 2024",
     location: "Jakarta, Indonesia",
     description:
       "Extention from associate degree to bachelor degree. Focused on software engineer, system design, machine learning, artificial intelligence, database system, math and analysis. Final thesis on building a Classification Model for Student Response to Lecture Topics Using the BERT Algorithm and Natural Language Processing (NLP).",
+    logo: "/binus.png",
   },
   {
     institution: "Politeknik Negeri Jakarta",
     degree: "Associate Degree of Electrical Engineering",
     field: "Electronics Engineering",
-    startDate: "Aug 2018",
-    endDate: "Jul 2021",
+    startDate: "Sept 2018",
+    endDate: "Aug 2021",
     location: "Depok, Indonesia",
     description:
       "Focused on Electronics Engineering, weak current system, python, database, instrumentation, analytical computation, algorithm and analysis. Final thesis on building a Implementation RFID and Barcode Scanner in Book Lending System in Libraries Integrated with Android.",
+    logo: "/pnj.jpg",
   },
 ];
